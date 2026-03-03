1c2de4807fd3cb9ed642c045d2ab2b339cdb9a2a
luis gomez <luisgomez@idetp.edu.co>
2026-03-03 10:49:21 -0500

    terminado

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
-import re
 
-print("Validación de contraseñas\n")
 
+print("=== Validación de usuario ===\n")
+
+
+correo = input("Ingrese su correo: ")
+
+patron_correo = r"^[^@]+@[^@]+\.[^@]+$"
+
+if re.match(patron_correo, correo):
+    print("Correo válido ✅\n")
+else:
+    print("Correo inválido ❌")
+    print("Debe tener texto antes y después del '@' y un punto después del '@'\n")
+    
 contraseña = input("Ingrese una contraseña: ")
 
 tiene_longitud = len(contraseña) >= 8
-tiene_mayuscula = re.search(r"[A-Z]", contraseña)
-tiene_minuscula = re.search(r"[a-z]", contraseña)
-tiene_numero = re.search(r"[0-9]", contraseña)
-tiene_especial = re.search(r"[!@#$%^&*(),.?\":{}|<>]", contraseña)
+tiene_mayuscula = bool(re.search(r"[A-Z]", contraseña))
+tiene_minuscula = bool(re.search(r"[a-z]", contraseña))
+tiene_numero = bool(re.search(r"[0-9]", contraseña))
+tiene_especial = bool(re.search(r"[!@#$%^&*(),.?\":{}|<>]", contraseña))
 
-if tiene_longitud and tiene_mayuscula and tiene_minuscula and tiene_numero and tiene_especial:
-    print(" Contraseña válida")
+if all([tiene_longitud, tiene_mayuscula, tiene_minuscula, tiene_numero, tiene_especial]):
+    print("Contraseña válida ✅")
 else:
-    print(" Contraseña inválida")
+    print("Contraseña inválida ❌")
     print("Debe cumplir con:")
 
     if not tiene_longitud:
-        print(" Debe tener al menos 8 caracteres")
+        print("- Debe tener al menos 8 caracteres")
     if not tiene_mayuscula:
-        print(" Al menos una letra mayúscula")
+        print("- Al menos una letra mayúscula")
     if not tiene_minuscula:
-        print(" Al menos una letra minúscula")
+        print("- Al menos una letra minúscula")
     if not tiene_numero:
-        print(" Al menos un número")
+        print("- Al menos un número")
     if not tiene_especial:
-        print(" Al menos un carácter especial")
+        print("- Al menos un carácter especial")
